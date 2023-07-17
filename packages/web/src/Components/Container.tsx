78bfe37907384a57efaf62e042fd82dec50e8bf9
Ji Chang <mrchangji@outlook.com>
2023-07-17 13:37:13 +0800

    Updates

--- packages/web/src/Components/Container.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentProps } from "react";
-import "./Container.css";
-
-export interface ContainerProps extends ComponentProps<"div"> {}
-
-export function Container(props: ContainerProps) {
-  return <div className="container">{props.children}</div>;
-}
